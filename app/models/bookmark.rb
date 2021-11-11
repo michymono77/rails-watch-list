0229336cbdd65abe4385bfea5680a1e1719c074d
Michiharu Ono <michiharuono77@gmail.com>
2021-11-11 12:02:46 +0900

    model complete

---
+++ app/models/bookmark.rb
@@ -0,0 +1,8 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, length: { minimum: 7 }, allow_blank: false
+  validates :movie, presence: true, uniqueness: { scope: :list }
+
+end
