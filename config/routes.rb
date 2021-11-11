d49475a7d78ce2763d3399bfa8e39deec0b282e4
Michiharu Ono <michiharuono77@gmail.com>
2021-11-12 07:32:17 +0900

    challenge complete

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   end
   resources :movies, only: [:index]
 
+  root to: "movies#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
