82e32fd0396c5b193a3e40c91324af30ab21640f
Michiharu Ono <michiharuono77@gmail.com>
2021-11-11 23:08:09 +0900

    challenge complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'movies/index'
   get 'bookmarks/new'
   resources :lists, only: [:index, :show, :new, :create ] do
     resources :bookmarks, only: [ :new, :create, :destroy]
   end
-
+  resources :movies, only: [:index]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,7 @@ movie = JSON.parse(movie_serialized)
 movies = movie["results"]
 
 movies.each do |movie|
-  Movie.create(title: movie["title"], overview: movie["poster_path"], poster_url: "https://image.tmdb.org/t/p/w500/#{movie["overview"]}", rating: movie["vote_average"])
+  Movie.create(title: movie["title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}", rating: movie["vote_average"])
 end
 
 
